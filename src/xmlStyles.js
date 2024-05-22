3f47f0e9c6203eba806f428a229a4979f7cac378
uncenter <47499684+uncenter@users.noreply.github.com>
2024-05-22 11:35:54 -0400

    chore: remove previous build script

--- src/xmlStyles.js
+++
@@ -1,9 +0,0 @@
-const { Eta } = require("eta");
-
-const eta = new Eta({ views: __dirname, useWith: true });
-
-const renderXmlStyles = (variantName, variantObj) => {
-  return eta.render("chroma-style.xml.eta", { variantName: variantName, variant: variantObj });
-};
-
-module.exports = renderXmlStyles;
